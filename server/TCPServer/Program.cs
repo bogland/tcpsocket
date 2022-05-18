7219479ca4710ec755c2880cafd48812768e30a4
oh <guruwang2@gmail.com>
2022-05-18 22:51:09 +0900

    소켓 TCP async 구현

--- server/TCPServer/Program.cs
+++ server/TCPServer/Program.cs
@@ -1,22 +1,90 @@
 ﻿using System.Net;
 using System.Net.Sockets;
+using System.Text;
+using System.Threading;
 
-Console.WriteLine("Hello, World!");
-var endPoint = new IPEndPoint(IPAddress.Any, 20000);
-var socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-socket.Bind(endPoint);
-socket.Listen(100); //N명까지 허락
-while (true)
+Server server = new Server();
+server.StartListening();
+class Server
 {
-    //SocketAsyncEventArgs arg = new SocketAsyncEventArgs();
-    //arg.Completed += OnAccept;
-    var socketClient = socket.Accept();
-    socketClient.Send(new byte[] { 0, 1 });
+    Socket listener;
+    byte[] buffer = new Byte[1024];
+    List<Socket> clientList = new List<Socket> ();
+    void AcceptCallback(IAsyncResult ar)
+    {
+        Console.WriteLine("accepted...");
+        Socket handler = listener.EndAccept(ar);
+        clientList.Add(handler);
+        StateObject state = new StateObject();
+        handler.BeginReceive(buffer, 0, buffer.Length, 0, new AsyncCallback(ReadCallback), state);
+    }
+    void ReadCallback(IAsyncResult ar)
+    {
+        string data = Encoding.ASCII.GetString(buffer, 0, buffer.Length);
+
+        Console.WriteLine("receive..." + data);
+        foreach(var client in clientList)
+        {
+            byte[] msg = Encoding.ASCII.GetBytes(data);
+            client.Send(msg);
+        }
+    }
+    public void StartListening()
+    {
+        string data = null;
+        // Data buffer for incoming data.  
+
+
+        // Establish the local endpoint for the socket.  
+        // Dns.GetHostName returns the name of the
+        // host running the application.  
+        IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+        IPAddress ipAddress = ipHostInfo.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        // Create a TCP/IP socket.  
+        listener = new Socket(ipAddress.AddressFamily,
+            SocketType.Stream, ProtocolType.Tcp);
+
+        // Bind the socket to the local endpoint and
+        // listen for incoming connections.  
+        try
+        {
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+            // Start listening for connections.  
+            while (true)
+            {
+                listener.BeginAccept(new AsyncCallback(AcceptCallback),listener);
+
+                //handler.Send(msg);
+                //handler.Shutdown(SocketShutdown.Both);
+                //handler.Close();
+            }
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress ENTER to continue...");
+        Console.Read();
+
+    }
 }
 
-//void OnAccept(object? sender, SocketAsyncEventArgs e)
-//{
-//    Console.WriteLine("{0} 접속됨", e.RemoteEndPoint.ToString());
-//    var client = e.AcceptSocket;
-//    Console.WriteLine("접속됨 {0}", client.RemoteEndPoint.ToString());
-//}
+public class StateObject
+{
+    // Size of receive buffer.  
+    public const int BufferSize = 1024;
+
+    // Receive buffer.  
+    public byte[] buffer = new byte[BufferSize];
+
+    // Received data string.
+    public StringBuilder sb = new StringBuilder();
+
+    // Client socket.
+    public Socket workSocket = null;
+}
